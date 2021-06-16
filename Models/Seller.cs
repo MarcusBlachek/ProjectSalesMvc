9fec3b7fc405c6fb59c393d7043438e5409b3488
MarcusBlachek <marcus_blachek@hotmail.com>
2021-06-16 19:25:01 -0300

    Adicionado os métodos AddSales, RemoveSales e TotalSales na classe Sellers

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -15,5 +15,34 @@ namespace ProjectSalesMvc.Models
 
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string senha, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Senha = senha;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial,DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
     }
 }
